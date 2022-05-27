e5d5e114a88e9f2aa0e1005e58073074375b0380
Aiperi Eldar <aiperieldar.dev@gmail.com>
2022-05-27 00:01:14 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import './App.css';
-import Navbar from './components/Navbar';
+import Navbar from './components/Navbar'
+import {BrowserRouter as Router} from 'react-router-dom'
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
-    <div>
-      <Navbar />
-    </div>
+    <Router>
+      <Sidebar />
+      <Navbar /> 
+    </Router> 
   );
 }
 
