06f99fd2cc44d4d1587e0e40cfd20ea7f58c8829
Aiperi Eldar <aiperieldar.dev@gmail.com>
2022-06-09 14:28:14 -0400

    main page

---
+++ src/components/InfoSection/Data.js
@@ -0,0 +1,16 @@
+export const homeObjOne = {
+    id: 'about',
+    light: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Yoga Getaway',
+    headline: 'Soulful week at the oceanhouse',
+    description: 'Get access to our exclusive community that allows you to relax and recharge at the getaway house by the ocean.',
+    buttonLabel: 'Get Started',
+    imgStart: false,
+    img: require('../../images/svg-1.svg'),
+    alt: 'Car',
+    dark: true,
+    primary: true,
+    darkText: false
+};
